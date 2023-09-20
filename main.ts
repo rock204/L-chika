5fb42201e3b7428a9900ecf51d084bc1bddd5129
rock204 <75961408+rock204@users.noreply.github.com>
2023-09-20 16:00:29 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,17 @@
+let bf = 0
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 10; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        basic.pause(500)
+    }
+})
+basic.forever(function () {
+    bf = pins.digitalReadPin(DigitalPin.P1)
+    if (bf == 1) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
